c78827ac80cf2e7616444cf3da97d3c9be2039e3
Tessaira <idawagenhofer9@gmail.com>
2020-01-07 12:40:45 +0100

    eradicated magical strings and values, applied camelcase,redundant condition removed in if statement, deleted namespaces, deleted blank lines, adjusted brackets

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Animator anim, anim2, anim3, anim4, anim5;
-    [SerializeField] float jumpForce;
-    
+    private Rigidbody2D rb;
+    private Animator anim;
+    private const string JUMP_TRIGGER_NAME = "Jump";
+    private const string DEATH_STATE_NAME = "SantaDeath";
+    private const string GROUND_TAG = "Ground";
+    private const string OBSTACLE_TAG = "Obstacle";
+    [SerializeField] private float jumpForce;
+
+    private bool grounded;
+    private bool gameOver;
 
     private void Awake()
     {
@@ -15,65 +19,42 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && !gameOver && !gameOver && !gameOver)
+        if (Input.GetMouseButton(0) && !gameOver)
         {
             if (grounded == true)
             {
-                jump();
+                Jump();
             }
-
-
-
-
         }
     }
 
-    bool grounded;
-    bool gameOver = false;
-
-    void jump()
+    private void Jump()
     {
         grounded = false;
-
         rb.velocity = Vector2.up * jumpForce;
-
-        anim.SetTrigger("Jump");
-
+        anim.SetTrigger(JUMP_TRIGGER_NAME);
         GameManager.instance.IncrementScore();
-        Debug.Log("DeleteMe");
     }
 
-    private bool SetGameOverTrue()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        return true;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)   {
-        if(collision.gameObject.tag == "Ground")
+        if (collision.gameObject.CompareTag(GROUND_TAG))
         {
-            grounded = true;}
+            grounded = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle"){
+        if (collision.gameObject.CompareTag(OBSTACLE_TAG))
+        {
             GameManager.instance.GameOver();
             Destroy(collision.gameObject);
-            anim.Play("SantaDeath");
-            gameOver = SetGameOverTrue();
+            anim.Play(DEATH_STATE_NAME);
+            gameOver = true;
         }
     }
-
-
-
-
 }
